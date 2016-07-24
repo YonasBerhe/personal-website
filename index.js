7f2bf50b0e8c44c1916a5cc2dcf13995b2bf57c6
YonasBerhe <yonas.berhe007@gmail.com>
2016-07-24 11:53:24 -0700

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict"
 
-let nodemailer, express, app, bodyParser, path, data, fs, creds, smtpTransport, goodreads;
+let nodemailer, express, app, bodyParser, path, data, fs, creds, smtpTransport, goodreads, transporter, options, mailOptions, key, secret, gr;
 
 
 nodemailer = require('nodemailer');
@@ -13,37 +13,10 @@ fs = require('fs');
 creds = fs.readFileSync('creds.json', 'utf8');
 //  console.log(creds)
 goodreads = require('goodreads');
-
+// trying requiring files for goodreads in this one
 
 key = creds.key;
 secret = creds.secret;
-
-
-gr = new goodreads.client({
-  'key': key,
-  'secret': secret
-});
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/books', function (req, res) {
-      
-gr.getSingleShelf({
-  'userID': '40688141',
-  'shelf': 'web',
-  'page': 1,
-  'per_page': 200
-}, function (json) {
-  if (json) {
-    res.send(json);
-    console.log(JSON.stringify(json));
-    // return res.end();
-  }
-});
-
-});
 
 app.use(function (req, res, next) {
   console.log(req.method, req.url);
@@ -65,62 +38,69 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 
-app.post('/data', function (req, res) {
-  console.log(req.body);
-  data = req.body;
+gr = new goodreads.client({
+  'key': key,
+  'secret': secret
+});
 
+app.get('/', function (req, res) {
+  res.send('Hello Server');
+});
 
+app.get('/books', function (req, res) {
 
-
-  // var transporter = nodemailer.createTransport('SMTP', {
-  //   service: 'gmail',
-  //   auth: {
-  //     user: 'yonas.berhe007@gmail.com',
-  //     pass: creds.pass
-  //   }
-  // });
-
-  // var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
-
-
-  var options = {
-    service: 'hotmail',
-    auth: {
-      user: 'ymberhe@outlook.com',
-      pass: 'Four81234206'
+  gr.getSingleShelf({
+    'userID': '40688141',
+    'shelf': 'web',
+    'page': 1,
+    'per_page': 200
+  }, function (json) {
+    if (json) {
+      res.send(json);
+      console.log(JSON.stringify(json));
+      // return res.end();
     }
-  };
-  var transporter = nodemailer.createTransport(smtpTransport(options))
-
-
-
-  // setup e-mail data with unicode symbols
-  var mailOptions = {
-    // check if you need these from
-    from: data.Email, // sender address
-    to: 'yonas.berhe007@gmail.com', // list of receivers
-    subject: 'Message from personal website', // Subject line
-    // text: // plaintext body
-    html: "</b>" + data.Name + "</br>" + data.Email + "</br> " + data.Message
-  };
-
-  // send mail with defined transport object
-  // transporter.sendMail(mailOptions, function(error, info) {
-  //   if (error) {
-  //     return console.log(error);
-  //   }
-  //   console.log('Message sent: ' + info.response);
-  // });
-
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
   });
-  // res.sendStatus(200);
-})
+
+});
+
+
+// app.post('/data', function (req, res) {
+//   console.log(req.body);
+//   data = req.body;
+
+
+//   options = {
+//     service: 'hotmail',
+//     auth: {
+//       user: 'ymberhe@outlook.com',
+//       pass: 'Four81234206'
+//     }
+//   };
+//   transporter = nodemailer.createTransport(smtpTransport(options))
+
+
+
+//   // setup e-mail data with unicode symbols
+//   mailOptions = {
+//     // check if you need these from
+//     from: data.Email, // sender address
+//     to: 'yonas.berhe007@gmail.com', // list of receivers
+//     subject: 'Message from personal website', // Subject line
+//     // text: // plaintext body
+//     html: "</b>" + data.Name + "</br>" + data.Email + "</br> " + data.Message
+//   };
+
+
+
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       return console.log(error);
+//     }
+//     console.log('Message sent: ' + info.response);
+//   });
+//   // res.sendStatus(200);
+// })
 
 
 app.listen(4812, function () {
