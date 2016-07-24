06559c8ad363ad38b921b9a007158af92db82ac7
YonasBerhe <yonas.berhe007@gmail.com>
2016-07-23 21:41:48 -0700

    .

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 "use strict"
 
-let nodemailer, express, app, bodyParser, path, data, fs, creds;
+let nodemailer, express, app, bodyParser, path, data, fs, creds, smtpTransport;
 
 
  nodemailer = require('nodemailer');
+ smtpTransport = require('nodemailer-smtp-transport');
  express = require('express');
  app = express();
  bodyParser = require('body-parser');
  path = require('path');
  fs = require('fs');
  creds = fs.readFileSync('creds.json', 'utf8');
- console.log(creds)
+//  console.log(creds)
 
 
 // var morgan = require('morgan');
@@ -40,34 +41,48 @@ app.post('/data', function(req, res) {
   console.log(req.body);
   data = req.body;
 
+//Check out node mailer well known servies!
+//  https://nodemailer.com/2-0-0-beta/setup-smtp/well-known-services/
 
-  var transporter = nodemailer.createTransport('SMTP', {
-    service: 'gmail',
+var options = {
+    service: 'EMAIL PROVIDER',
     auth: {
-      user: 'your email',
-      pass: creds.pass
+        user: 'YOUREMAIL',
+        pass: 'YOURPASS WORD'
     }
-  });
+  };
+  var transporter = nodemailer.createTransport(smtpTransport(options))
+
+
 
   // setup e-mail data with unicode symbols
   var mailOptions = {
     // check if you need these from
     from: data.Email, // sender address
-    to: 'ymberhe@outlook.com', // list of receivers
+    to: 'yonas.berhe007@gmail.com', // list of receivers
     subject: 'Message from personal website', // Subject line
     // text: // plaintext body
     html: "</b>" + data.Name +  "</br>" + data.Email + "</br> " +   data.Message 
   };
 
   // send mail with defined transport object
-  transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-      return console.log(error);
+  // transporter.sendMail(mailOptions, function(error, info) {
+  //   if (error) {
+  //     return console.log(error);
+  //   }
+  //   console.log('Message sent: ' + info.response);
+  // });
+
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
     }
     console.log('Message sent: ' + info.response);
-  });
+});
   // res.sendStatus(200);
 })
+
 
 app.listen(4812, function() {
   console.log("server has started on port 4812");
